a3434acd139d72181f081bda5314c55b7e244847
Dahan Schuster <dan.plschuster@gmail.com>
2022-12-04 18:57:17 -0300

    fix: properties names in avatars object

--- src/Components/PlayerAvatar/avatars.js
+++ src/Components/PlayerAvatar/avatars.js
@@ -1,14 +1,14 @@
 const avatarImages = {
-	0: require(`../../../assets/avatars/avatar_1.png`),
-	1: require(`../../../assets/avatars/avatar_1.png`),
-	2: require(`../../../assets/avatars/avatar_2.png`),
-	3: require(`../../../assets/avatars/avatar_3.png`),
-	4: require(`../../../assets/avatars/avatar_4.png`),
-	5: require(`../../../assets/avatars/avatar_5.png`),
-	6: require(`../../../assets/avatars/avatar_6.png`),
-	7: require(`../../../assets/avatars/avatar_7.png`),
-	8: require(`../../../assets/avatars/avatar_8.png`),
-	9: require(`../../../assets/avatars/avatar_9.png`),
+	avatar_0: require(`../../../assets/avatars/avatar_1.png`),
+	avatar_1: require(`../../../assets/avatars/avatar_1.png`),
+	avatar_2: require(`../../../assets/avatars/avatar_2.png`),
+	avatar_3: require(`../../../assets/avatars/avatar_3.png`),
+	avatar_4: require(`../../../assets/avatars/avatar_4.png`),
+	avatar_5: require(`../../../assets/avatars/avatar_5.png`),
+	avatar_6: require(`../../../assets/avatars/avatar_6.png`),
+	avatar_7: require(`../../../assets/avatars/avatar_7.png`),
+	avatar_8: require(`../../../assets/avatars/avatar_8.png`),
+	avatar_9: require(`../../../assets/avatars/avatar_9.png`),
 }
 
 export default avatarImages
