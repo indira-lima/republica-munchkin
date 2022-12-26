061ed829e5cac4cca9ef0c5b0cd03c1ee4405b5b
Dahan Schuster <dan.plschuster@gmail.com>
2022-12-26 19:31:44 -0300

    feat: Enable edition for PlayerAvatar

--- src/Components/Player/Avatar/index.js
+++ src/Components/Player/Avatar/index.js
@@ -1,37 +1,85 @@
-import {useMemo} from 'react'
-import {StyleSheet, View} from 'react-native'
-import FastImage from 'react-native-fast-image'
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { useCallback, useMemo } from "react";
+import { StyleSheet, TouchableWithoutFeedback, View } from "react-native";
+import FastImage from "react-native-fast-image";
 
-import {circulo} from '../../../Utils/styles'
+import globalStyles, { circulo } from "../../../Utils/styles";
 
-import avatarImages from './avatars'
+import avatarImages, { lastAvatarIndex } from "./avatars";
 
-const PlayerAvatar = ({ player, theme }) => {
-	const avatarSource = useMemo(() => {
-		const avatar = avatarImages[player?.avatar]
-		return avatar || avatarImages[0]
-	}, [player])
+const PlayerAvatar = ({
+  player,
+  theme,
+  enableEdit = false,
+  onChange = () => {},
+}) => {
+  const [avatarSource, currentAvatarIndex] = useMemo(() => {
+    const avatar = avatarImages[player?.avatar];
+    const source = avatar || avatarImages.avatar_0;
+    const index = Object.entries(avatarImages).findIndex(
+      ([, img]) => img === source
+    );
+
+    return [source, index];
+  }, [player]);
+
+  const handlePrevAvatar = useCallback(() => {
+    if (!enableEdit) return;
+
+    let newAvatarIndex;
+    if (currentAvatarIndex === 0) {
+      newAvatarIndex = lastAvatarIndex;
+    } else {
+      newAvatarIndex = currentAvatarIndex - 1;
+    }
+
+    onChange(`avatar_${newAvatarIndex}`);
+  }, [currentAvatarIndex]);
+
+  const handleNextAvatar = useCallback(() => {
+    if (!enableEdit) return;
+
+    let newAvatarIndex;
+
+    if (currentAvatarIndex === lastAvatarIndex) {
+      newAvatarIndex = 0;
+    } else {
+      newAvatarIndex = currentAvatarIndex + 1;
+    }
+
+    onChange(`avatar_${newAvatarIndex}`);
+  }, [currentAvatarIndex]);
 
   return (
-    <View>
+    <View style={styles.container}>
+      {enableEdit && (
+        <TouchableWithoutFeedback onPress={handlePrevAvatar}>
+          <MaterialCommunityIcons color="#fff" name="chevron-left" size={24} />
+        </TouchableWithoutFeedback>
+      )}
       <FastImage
         source={avatarSource}
-        style={[
-          styles.avatarImage,
-          { borderColor: theme.colors.primary },
-        ]}
+        style={[styles.avatarImage, { borderColor: theme.colors.primary }]}
       />
+      {enableEdit && (
+        <TouchableWithoutFeedback onPress={handleNextAvatar}>
+          <MaterialCommunityIcons color="#fff" name="chevron-right" size={24} />
+        </TouchableWithoutFeedback>
+      )}
     </View>
-  )
-}
+  );
+};
 
-const avatarSize = 78
+const avatarSize = 78;
 
 const styles = StyleSheet.create({
   avatarImage: {
     ...circulo(avatarSize),
     borderWidth: 3,
-  }
-})
+  },
+  container: {
+    ...globalStyles.row,
+  },
+});
 
-export default PlayerAvatar
+export default PlayerAvatar;
