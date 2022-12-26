e8c364c930974eb3d7426c45fcdc493c3dc5b625
Dahan Schuster <dan.plschuster@gmail.com>
2022-12-26 12:50:53 -0300

    wip: Player form within PlayerModal

--- src/Components/Player/Modal/index.jsx
+++ src/Components/Player/Modal/index.jsx
@@ -1,13 +1,33 @@
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 import ModalContainer from "../../ModalContainer";
 import styles from "./styles";
 
+import globalStyles from "../../../Utils/styles";
+import Button from "../../Button";
+import PlayerAvatar from "../Avatar";
+import {useState} from "react";
+import {themes} from "../utils/themes";
+
 const PlayerModal = ({ currentPlayer, ...modalProps }) => {
+
+	const _theme = themes[currentPlayer?.theme] || themes.default
+	const [theme, setTheme] = useState(_theme)
+	const [avatar, setAvatar] = useState(currentPlayer?.avatar)
+
   return (
     <ModalContainer {...modalProps}>
-      <Text style={styles.text}>
-        {currentPlayer ? currentPlayer.name : "New player"}
-      </Text>
+      <View style={styles.container}>
+        <Text style={styles.text}>
+          {currentPlayer ? currentPlayer.name : "New player"}
+        </Text>
+				<View style={[globalStyles.row]}>
+					<PlayerAvatar player={currentPlayer} theme={theme}/>
+				</View>
+        <View style={[globalStyles.row, styles.rowButtons]}>
+					<Button theme='cancel' type='squared' icon='close' />
+					<Button theme='deefault' type='squared' icon='check' style={{ marginLeft: 5, }}/>
+        </View>
+      </View>
     </ModalContainer>
   );
 };
--- src/Components/Player/Modal/styles.js
+++ src/Components/Player/Modal/styles.js
@@ -6,6 +6,17 @@ const styles = StyleSheet.create({
     ...globalStyles.text,
     color: "#fff",
   },
+	container: {
+		flex: 1,
+		justifyContent: 'space-between',
+		paddingVertical: 25,
+		paddingHorizontal: 15,
+		// borderColor: 'red', borderWidth: 1,
+	},
+	rowButtons: {
+		justifyContent: 'flex-end',
+		width: '100%',
+	}
 });
 
 export default styles;
