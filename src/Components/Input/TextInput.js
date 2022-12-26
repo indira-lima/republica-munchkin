1c945780f44bc0497dba8164074ed0ae7ee275ff
Dahan Schuster <dan.plschuster@gmail.com>
2022-12-26 19:31:21 -0300

    feat: TextInput styles

--- src/Components/Input/TextInput.js
+++ src/Components/Input/TextInput.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import { TextInput as RNTextInput, View, Text } from 'react-native';
+import React from "react";
+import { TextInput as RNTextInput, View, Text } from "react-native";
 
-import ErrorMessage from './ErrorMessage';
-import Label from './Label';
-import styles from './styles';
+import ErrorMessage from "./ErrorMessage";
+import Label from "./Label";
+import styles from "./styles";
 
-const TextInput = ({ placeholder, error, label, touched, ...otherProps }, ref) => {
-	
-	return (
-		<View style={styles.container}>
-			{label && <Label text={label}/>}
-			<RNTextInput
-				placeholder={placeholder}
-				style={[styles.input, touched && error ? styles.error : {}]}
-				ref={ref}
-				{...otherProps}
-			/>
-			{touched && error && <ErrorMessage message={error} />}
-		</View>
-	);
-}
+const TextInput = (
+  { placeholder, error, label, touched, ...otherProps },
+  ref
+) => {
+  return (
+    <View style={styles.container}>
+      {label && <Label text={label} />}
+      <RNTextInput
+        placeholder={placeholder}
+        placeholderTextColor={"#ebebeb"}
+        style={[styles.input, touched && error ? styles.error : {}]}
+        ref={ref}
+        {...otherProps}
+      />
+      {touched && error && <ErrorMessage message={error} />}
+    </View>
+  );
+};
 
-export default React.forwardRef(TextInput)
+export default React.forwardRef(TextInput);
+
