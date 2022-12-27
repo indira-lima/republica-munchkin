e8a343e6b8b31d005bed1873dd4a77af0e9e0dc3
Dahan Schuster <dan.plschuster@gmail.com>
2022-12-27 13:31:59 -0300

    feat: Player modal with a form to add/edit data

--- src/Components/Player/Modal/index.jsx
+++ src/Components/Player/Modal/index.jsx
@@ -1,31 +1,115 @@
-import { Text, View } from "react-native";
+import { View } from "react-native";
 import ModalContainer from "../../ModalContainer";
 import styles from "./styles";
 
+import { useCallback, useEffect, useMemo, useState } from "react";
+import useGame from "../../../Hooks/useGame";
 import globalStyles from "../../../Utils/styles";
 import Button from "../../Button";
+import TextInput from "../../Input/TextInput";
 import PlayerAvatar from "../Avatar";
-import {useState} from "react";
-import {themes} from "../utils/themes";
+import ChangeGenderBtn from "../ChangeGenderBtn";
+import ChangeThemeBtn from "../ChangeThemeBtn";
+import { themes } from "../utils/themes";
 
-const PlayerModal = ({ currentPlayer, ...modalProps }) => {
+/**
+ * Modal that opens a form to edit the player's data or to create a new one
+ */
+const PlayerModal = ({
+  currentPlayer = {},
+  onClose = () => {},
+  ...modalProps
+}) => {
+  // gets the functions to add/edit players from the context
+  const { addPlayer, editPlayer } = useGame();
 
-	const _theme = themes[currentPlayer?.theme] || themes.default
-	const [theme, setTheme] = useState(_theme)
-	const [avatar, setAvatar] = useState(currentPlayer?.avatar)
+  // stores the form data in a separate state
+  const [playerData, setPlayerData] = useState(null);
+
+  // gets the theme from the playerData
+  const theme = useMemo(
+    () => themes[playerData?.theme] || themes[0],
+    [playerData]
+  );
+
+  /**
+   * Initializes the playerData state with the currentPlayer prop
+   * when the modal opens, and clear the state when the modal closes
+   */
+  useEffect(() => {
+    setPlayerData(modalProps?.openModal ? currentPlayer : null);
+  }, [modalProps?.openModal]);
+
+  /**
+   * Function to update one prop of the playerData state
+   * Used in each field of the form
+   */
+  const handleSetPlayerValue = useCallback((propName, value) => {
+    setPlayerData((data) => ({
+      ...data,
+      [propName]: value,
+    }));
+  }, []);
+
+  /**
+   * Create or edit a player with the current form data
+   * and then closes the modal
+   */
+  const handleSavePlayer = useCallback(() => {
+    if (playerData.id) {
+      editPlayer(playerData.id, playerData);
+    } else {
+      addPlayer(playerData);
+    }
+
+    onClose();
+  }, [playerData]);
 
   return (
     <ModalContainer {...modalProps}>
       <View style={styles.container}>
-        <Text style={styles.text}>
-          {currentPlayer ? currentPlayer.name : "New player"}
-        </Text>
-				<View style={[globalStyles.row]}>
-					<PlayerAvatar player={currentPlayer} theme={theme}/>
-				</View>
+        <View style={styles.formContent}>
+          <View style={[globalStyles.row]}>
+            <ChangeThemeBtn
+              player={playerData}
+              theme={theme}
+              onChange={(value) => handleSetPlayerValue("theme", value)}
+            />
+            <PlayerAvatar
+              enableEdit
+              theme={theme}
+              player={playerData}
+              onChange={(value) => handleSetPlayerValue("avatar", value)}
+            />
+            <ChangeGenderBtn
+              player={playerData}
+              theme={theme}
+              onChange={(value) => handleSetPlayerValue("gender", value)}
+            />
+          </View>
+          <View style={styles.inputSession}>
+            <TextInput
+              label={">"}
+              value={playerData?.name}
+              onChangeText={(value) => handleSetPlayerValue("name", value)}
+              placeholder="Username"
+            />
+          </View>
+        </View>
         <View style={[globalStyles.row, styles.rowButtons]}>
-					<Button theme='cancel' type='squared' icon='close' />
-					<Button theme='deefault' type='squared' icon='check' style={{ marginLeft: 5, }}/>
+          <Button
+            theme="cancel"
+            type="squared"
+            icon="close"
+            onPress={onClose}
+          />
+          <Button
+            theme="default"
+            type="squared"
+            icon="check"
+            style={{ marginLeft: 5 }}
+            onPress={handleSavePlayer}
+          />
         </View>
       </View>
     </ModalContainer>
--- src/Components/Player/Modal/styles.js
+++ src/Components/Player/Modal/styles.js
@@ -2,21 +2,30 @@ import { StyleSheet } from "react-native";
 import globalStyles from "../../../Utils/styles";
 
 const styles = StyleSheet.create({
-  text: {
+  title: {
     ...globalStyles.text,
-    color: "#fff",
+    fontSize: 20,
   },
-	container: {
-		flex: 1,
-		justifyContent: 'space-between',
-		paddingVertical: 25,
-		paddingHorizontal: 15,
-		// borderColor: 'red', borderWidth: 1,
+  container: {
+    flex: 1,
+    justifyContent: "space-between",
+    paddingVertical: 25,
+    paddingHorizontal: 15,
+    // borderColor: 'red', borderWidth: 1,
+  },
+  rowButtons: {
+    justifyContent: "flex-end",
+    width: "100%",
+  },
+	formContent: {
+		justifyContent: 'center',
+		alignItems: 'center'
 	},
-	rowButtons: {
-		justifyContent: 'flex-end',
+  inputSession: {
+		justifyContent: 'center',
+		alignItems: 'center',
 		width: '100%',
-	}
+  },
 });
 
 export default styles;
