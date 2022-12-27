7d3b01b543565727a49762c175f1aa4cb5dbd262
Dahan Schuster <dan.plschuster@gmail.com>
2022-12-27 13:04:05 -0300

    feat: Buttons to change the player's gender

---
+++ src/Components/Player/ChangeGenderBtn/index.jsx
@@ -0,0 +1,46 @@
+import { useCallback, useMemo } from "react";
+import { Genders } from "../../../Utils/static";
+import Button from "../../Button";
+import genderIcons from "../utils/genders";
+
+/**
+ * Button to change the player's gender
+ * Should receive a onChange function that handles changes in the gender value
+ */
+const ChangeGenderBtn = ({ player, onChange = () => {}, theme }) => {
+
+	/**
+	 * Gets the icon source and index from the player.gender value
+	 */
+  const [genderImgSource, currentGenderIndex] = useMemo(() => {
+    const source = genderIcons[player?.gender] || genderIcons[Genders.PAN];
+    const index = Object.entries(genderIcons).findIndex(([, img]) => img === source);
+
+    return [source, index];
+  }, [player?.gender]);
+
+	/**
+	 * Changes the current gender value to the next one in the Genders list
+	 */
+  const handleChangeGender = useCallback(() => {
+		let newGenderIndex
+		if (currentGenderIndex === Object.keys(Genders).length - 1) {
+			newGenderIndex = 0
+		} else {
+			newGenderIndex = currentGenderIndex + 1
+		}
+		
+		onChange(newGenderIndex)
+	}, [currentGenderIndex]);
+
+  return (
+    <Button
+      type="hexagon"
+      theme="default"
+      icon={genderImgSource}
+      onPress={handleChangeGender}
+    />
+  );
+};
+
+export default ChangeGenderBtn;
