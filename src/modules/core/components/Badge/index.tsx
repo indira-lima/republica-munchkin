46ac6e92fd9a6ddf9fa2a9820b6a0c86fafdeb2e
Dahan Schuster <dan.plschuster@gmail.com>
2023-01-31 01:09:31 -0300

    refact: ts-migration for Badge component

--- src/modules/core/components/Badge/index.tsx
+++ src/modules/core/components/Badge/index.tsx
@@ -3,31 +3,48 @@ import { StyleSheet, Text } from "react-native";
 import globalStyles, { colors } from "../../utils/styles";
 import IconText from "../IconText";
 
-// @ts-expect-error TS(2339): Property 'children' does not exist on type '{}'.
-const Badge = React.memo(({ children, theme, icon, ...textProps }) => {
-// @ts-expect-error TS(7053): Element implicitly has an 'any' type because expre... Remove this comment to see the full error message
-	const style = {...styles.badge, backgroundColor: colors[theme] || colors.primary }
+interface BadgeProps {
+  theme: string;
+  icon?: string;
+  children?: React.ReactNode;
+}
 
-	if (icon) {
-		return (
-			<IconText icon={icon} style={style} {...textProps}>
-				{children}
-			</IconText>
-		)
-	}
+const Badge: React.FunctionComponent<BadgeProps> = ({
+  children,
+  theme,
+  icon,
+  ...textProps
+}) => {
+  const style = {
+    ...styles.badge,
+    // @ts-expect-error
+    backgroundColor: colors[theme] || colors.primary,
+  };
 
-	return <Text style={style} {...textProps}>{children}</Text>
-})
+  if (icon) {
+    return (
+      <IconText icon={icon} style={style} {...textProps}>
+        {children}
+      </IconText>
+    );
+  }
+
+  return (
+    <Text style={style} {...textProps}>
+      {children}
+    </Text>
+  );
+};
+
+export default React.memo(Badge);
 
 const styles = StyleSheet.create({
-	badge: {
-		...globalStyles.text,
-		textAlign: 'center',
-		color: 'white',
-		paddingVertical: 3,
-		paddingHorizontal: 4.8,
-		borderRadius: 4,
-	}
+  badge: {
+    ...globalStyles.text,
+    textAlign: "center",
+    color: "white",
+    paddingVertical: 3,
+    paddingHorizontal: 4.8,
+    borderRadius: 4,
+  },
 });
-
-export default Badge
