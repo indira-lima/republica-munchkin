260ba692e1cb10583808419aedce6d542023dd87
Dahan Schuster <dan.plschuster@gmail.com>
2023-02-10 16:51:50 -0300

    refact: Organize import in the AppRoutes

--- src/modules/core/routes/App/index.tsx
+++ src/modules/core/routes/App/index.tsx
@@ -1,65 +1,63 @@
-import { createStackNavigator } from '@react-navigation/stack';
+import { createStackNavigator } from "@react-navigation/stack";
 
-import GameScreen from '../../../game/screens/Main';
-import PlayersScreen from '../../../players/screens/Main';
-import BattleScreen from '../../../battle/screens/Main';
-import Header from '../../containers/Header';
-import {Player} from '../../definitions';
+import BattleScreen from "../../../battle/screens/Main";
+import GameScreen from "../../../game/screens/Main";
+import PlayersScreen from "../../../players/screens/Main";
+import Header from "../../containers/Header";
+import { Player } from "../../definitions";
 
-const fade = ({
-    current,
-}: any) => {
+const fade = ({ current }: any) => {
   return {
-		cardStyle: {
-			opacity: current.progress,
-		},
-	}
+    cardStyle: {
+      opacity: current.progress,
+    },
+  };
 };
 
 type AppStackParamList = {
-	Game: undefined;
-	Battle: { player: Player };
-	Players: undefined;
-}
+  Game: undefined;
+  Battle: { player: Player };
+  Players: undefined;
+};
 
 const AppStack = createStackNavigator<AppStackParamList>();
 export default function AppRoutes() {
-	return (
-		<AppStack.Navigator
-			initialRouteName='Game'
-			screenOptions={{
-				headerShown: false,
-				animationEnabled: true,
-				cardStyleInterpolator: fade
-			}}
-		>
-			<AppStack.Screen
-				name="Game"
-				component={GameScreen}
-				options={{
-					headerShown: true,
+  return (
+    <AppStack.Navigator
+      initialRouteName="Game"
+      screenOptions={{
+        headerShown: false,
+        animationEnabled: true,
+        cardStyleInterpolator: fade,
+      }}
+    >
+      <AppStack.Screen
+        name="Game"
+        component={GameScreen}
+        options={{
+          headerShown: true,
           headerTransparent: true,
-					header: () => <Header />,
-				}}
-			/>
-			<AppStack.Screen
-				name="Battle"
-				component={BattleScreen}
-				options={{
-					headerShown: true,
+          header: () => <Header />,
+        }}
+      />
+      <AppStack.Screen
+        name="Battle"
+        component={BattleScreen}
+        options={{
+          headerShown: true,
           headerTransparent: true,
-					header: () => <Header />,
-				}}
-			/>
-			<AppStack.Screen
-				name="Players"
-				component={PlayersScreen}
-				options={{
-					headerShown: true,
+          header: () => <Header />,
+        }}
+      />
+      <AppStack.Screen
+        name="Players"
+        component={PlayersScreen}
+        options={{
+          headerShown: true,
           headerTransparent: true,
-					header: () => <Header />,
-				}}
-			/>
-		</AppStack.Navigator>
-	)
+          header: () => <Header />,
+        }}
+      />
+    </AppStack.Navigator>
+  );
 }
