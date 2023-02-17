d5de08ecc205c3389c238b34a8676cc9241366af
Dahan Schuster <dan.plschuster@gmail.com>
2023-02-17 18:33:51 -0300

    wip: New Game button
    Add a big Start button at the Game screen if no match is currently in
    progress
    The button should open a list of the crew members for choosing the
    players, which will then create the new match with the selected members

--- src/modules/game/screens/Main.tsx
+++ src/modules/game/screens/Main.tsx
@@ -1,4 +1,6 @@
 import { useCallback } from "react";
+import { Text, TouchableWithoutFeedback, View } from "react-native";
+import FastImage from "react-native-fast-image";
 import { FlatList } from "react-native-gesture-handler";
 
 import MainContainer from "../../core/containers/MainContainer";
@@ -6,8 +8,17 @@ import PanelPlayer from "../containers/PlayerPanel";
 
 import useGame from "../../core/hooks/useGame";
 
+// @ts-ignore
+import BigStartButton from "../../../../assets/BigStartButton.png";
+import useCrew from "../../core/hooks/useCrew";
+
 const GameScreen = () => {
-  const { playerList } = useGame();
+  const { crew } = useCrew();
+  const { isGameInProgress, createNewGame, playerList } = useGame();
+
+  const handleStart = useCallback(() => {
+    createNewGame([ crew[0]!, crew[0]!, crew[0]! ]);
+  }, []);
 
   const renderItemPlayer = useCallback(({ item }: any) => {
     return <PanelPlayer key={item.id} player={item} />;
@@ -15,12 +26,26 @@ const GameScreen = () => {
 
   return (
     <MainContainer>
-      <FlatList
-        contentContainerStyle={{ alignItems: "center" }}
-        data={playerList}
-        renderItem={renderItemPlayer}
-        keyExtractor={(_item, index) => `player ${index}`}
-      />
+      {isGameInProgress && (
+        <FlatList
+          contentContainerStyle={{ alignItems: "center" }}
+          data={playerList}
+          renderItem={renderItemPlayer}
+          keyExtractor={(_item, index) => `player ${index}`}
+        />
+      )}
+      {!isGameInProgress && (
+        <View
+          style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+        >
+          <TouchableWithoutFeedback onPress={handleStart}>
+            <FastImage
+              source={BigStartButton}
+              style={{ width: 250, height: 250 }}
+            />
+          </TouchableWithoutFeedback>
+        </View>
+      )}
     </MainContainer>
   );
 };
