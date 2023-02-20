c1c2ce26788f082d86a9919c5e5a11e375066354
Dahan Schuster <dan.plschuster@gmail.com>
2023-02-20 18:05:44 -0300

    refact: make the AnimatedPanel animation simplier

--- src/modules/core/containers/AnimatedPanel/index.tsx
+++ src/modules/core/containers/AnimatedPanel/index.tsx
@@ -5,7 +5,6 @@ import Animated, {
   runOnJS,
   useAnimatedStyle,
   useSharedValue,
-  withDelay,
   withTiming,
 } from "react-native-reanimated";
 import styles from "./styles";
@@ -24,8 +23,7 @@ const AnimatedPanel: React.FunctionComponent<AnimatedPanelProps> = ({
   isPanelOPen,
   onClose,
 }) => {
-  const zoomAnimation = useSharedValue(0);
-  const openingAnimation = useSharedValue(0);
+  const animation = useSharedValue(0);
   const [containerMaxHeight, setContainerMaxHeight] = useState<number>(0);
 
   /**
@@ -36,13 +34,11 @@ const AnimatedPanel: React.FunctionComponent<AnimatedPanelProps> = ({
    * the containerMaxHeight
    */
   useEffect(() => {
-    console.log("panel open:", isPanelOPen);
+		console.log("panel open:", isPanelOPen);
     if (isPanelOPen) {
-      zoomAnimation.value = withTiming(1, { duration: 200 }, () => {
-        openingAnimation.value = withDelay(200, withTiming(containerMaxHeight));
-      });
+      animation.value = withTiming(containerMaxHeight);
     } else {
-      openingAnimation.value = withTiming(0, {}, async () => {
+      animation.value = withTiming(0, {}, () => {
         if (typeof onClose === "function") {
           runOnJS(onClose)();
         }
@@ -57,39 +53,21 @@ const AnimatedPanel: React.FunctionComponent<AnimatedPanelProps> = ({
     setContainerMaxHeight(event.nativeEvent.layout.height);
   }, []);
 
-	/**
-	 * Sets the panel scale based on the zoomAnimation value
-	 * It gives the initial zoom in effect
-	 */
-	const panelZoomStyle = useAnimatedStyle(() => {
-		return {
-			transform: [{ scale: zoomAnimation.value }],
-		};
-	});
-
   /**
-   * Sets the container's max height based on the
-   * openingAnimation value, giving a vertical opening effect
+   * Sets the container's max height and opacity based on the
+   * animation value, giving a vertical opening effect
    */
-  const panelHeightStyle = useAnimatedStyle(() => {
+  const animatedStyles = useAnimatedStyle(() => {
     return {
-      maxHeight: openingAnimation.value,
-    };
-  });
-
-  /**
-   * Sets the opacity based on the openingAnimation value
-   */
-  const opacityStyle = useAnimatedStyle(() => {
-    return {
-      opacity: interpolate(openingAnimation.value, [0, 600], [0, 1]),
+      maxHeight: animation.value,
+      opacity: interpolate(animation.value, [0, containerMaxHeight], [0, 1]),
     };
   });
 
   return (
     <View style={styles.container} onLayout={handleOnContainerLayout}>
-      <Animated.View style={[styles.crewSelectionContainer, panelZoomStyle, panelHeightStyle]}>
-        <Animated.View style={[opacityStyle]}>{children}</Animated.View>
+      <Animated.View style={[styles.crewSelectionContainer, animatedStyles]}>
+        {children}
       </Animated.View>
     </View>
   );
