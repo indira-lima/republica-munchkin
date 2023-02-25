f801be4fefbc7d95fc21007523a0e172c6b27854
Dahan Schuster <dan.plschuster@gmail.com>
2023-02-25 14:43:10 -0300

    feat: show app version at the Footer

--- src/modules/core/containers/Footer/index.tsx
+++ src/modules/core/containers/Footer/index.tsx
@@ -1,84 +1,111 @@
-import React, { useCallback, useMemo } from 'react'
-import { StyleSheet, TouchableOpacity, View} from 'react-native'
-import { useNavigation, useRoute } from '@react-navigation/native'
-import IonIcons from '@expo/vector-icons/Ionicons'
+import IonIcons from "@expo/vector-icons/Ionicons";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import React, { useCallback, useMemo } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
-import { circulo, colors } from '../../utils/styles'
+import globalStyles, { colors } from "../../utils/styles";
 
-const Footer = () => {
-	// navigation
-	const navigation = useNavigation()
-	const route = useRoute()
+import { version } from "../../../../app_config.json";
 
-	/**
-	 * Return an object to configure a footer item with navigation action
-	 */
-// @ts-expect-error TS(7006): Parameter 'routeName' implicitly has an 'any' type... Remove this comment to see the full error message
-	const getLinkConfig = useCallback((routeName, params = {}) => {
-		return {
-			active: route.name === routeName,
-			onPress: () => {
-// @ts-expect-error TS(2345): Argument of type 'any' is not assignable to parame... Remove this comment to see the full error message
-				navigation.navigate(routeName, params)
-			}
-		}
-	}, [route, navigation])
-	
-	const footerItems = useMemo(() => [
-		{ 
-			order: 1,
-			label: 'Game',
-			icon: 'game-controller',
-			...getLinkConfig('Game')
-		},
-		{
-			order: 2,
-			label: 'Crew',
-			icon: 'people',
-			...getLinkConfig('Crew')
-		},
-	], [navigation, getLinkConfig])
-
-
-  	return (
-		<View style={styles.tabBar}>
-			{footerItems
-// @ts-expect-error TS(2551): Property 'sortBy' does not exist on type '{ active... Remove this comment to see the full error message
-				.sortBy('order')
-// @ts-expect-error TS(7031): Binding element 'label' implicitly has an 'any' ty... Remove this comment to see the full error message
-				.map(({ label, icon, onPress, active = false }) => (
-					<TouchableOpacity
-						key={label}
-						accessibilityRole="button"
-						accessibilityLabel={label}
-						style={[styles.tabBarButton]}
-						onPress={onPress}
-						disabled={active}
-					>
-						<IonIcons name={icon} size={iconSize} color={active ? colors.primary : colors.text} />
-					</TouchableOpacity>
-				))
-			}
-	  </View>
-	)
+type LinkConfig = {
+  active: boolean;
+  onPress: () => void;
 };
 
-const iconSize = 36
+interface FooterItem extends LinkConfig {
+  order: number;
+  label: string;
+  icon: string;
+}
+
+const Footer = () => {
+  // navigation
+  const navigation = useNavigation();
+  const route = useRoute();
+
+  /**
+   * Return an object to configure a footer item with navigation action
+   */
+  const getLinkConfig = useCallback(
+    (routeName: string, params = {}): LinkConfig => {
+      return {
+        active: route.name === routeName,
+        onPress: () => {
+          // @ts-ignore
+          navigation.navigate(routeName, params);
+        },
+      };
+    },
+    [route, navigation]
+  );
+
+  const footerItems = useMemo<FooterItem[]>(
+    () => [
+      {
+        order: 1,
+        label: "Game",
+        icon: "game-controller",
+        ...getLinkConfig("Game"),
+      },
+      {
+        order: 2,
+        label: "Crew",
+        icon: "people",
+        ...getLinkConfig("Crew"),
+      },
+    ],
+    [navigation, getLinkConfig]
+  );
+
+  return (
+    <View style={styles.footerContainer}>
+      <View style={styles.tabBar}>
+        {footerItems
+          // @ts-ignore
+          .sortBy("order")
+          .map(({ label, icon, onPress, active = false }: FooterItem) => (
+            <TouchableOpacity
+              key={label}
+              accessibilityRole="button"
+              accessibilityLabel={label}
+              style={[styles.tabBarButton]}
+              onPress={onPress}
+              disabled={active}
+            >
+              <IonIcons
+                // @ts-ignore
+                name={icon}
+                size={iconSize}
+                color={active ? colors.primary : colors.text}
+              />
+            </TouchableOpacity>
+          ))}
+      </View>
+      <Text style={styles.versionText}>{version}</Text>
+    </View>
+  );
+};
+
+const iconSize = 36;
 const styles = StyleSheet.create({
-	tabBar: {
-		flexDirection: 'row',
-		justifyContent: 'space-around',
-		paddingVertical: 8,
-		backgroundColor: `${colors.primary}50`,
-	},
-	tabBarButton: {
-		alignItems: 'center',
-	},
-	tabBarButtonLabel: {
-		color: '#fff',
-		textAlign: 'center',
-		fontSize: 10,
-	}
+  footerContainer: { backgroundColor: `${colors.primary}50`, paddingBottom: 4 },
+  tabBar: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    paddingVertical: 8,
+  },
+  tabBarButton: {
+    alignItems: "center",
+  },
+  tabBarButtonLabel: {
+    color: "#fff",
+    textAlign: "center",
+    fontSize: 10,
+  },
+  versionText: {
+    ...globalStyles.text,
+    fontSize: 10,
+  },
 });
 
-export default Footer
+export default Footer;
