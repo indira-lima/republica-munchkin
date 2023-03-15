aa535572b956c78050a32fab68320ead06e1882b
Dahan Schuster <dan.plschuster@gmail.com>
2023-03-15 01:08:11 -0300

    feat: Component for showing info about the fighter
    The component can render the main and the ally player, and a button to
    call an ally if not present

---
+++ src/modules/battle/containers/Fighter/index.tsx
@@ -0,0 +1,70 @@
+import React, { Fragment } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import AvatarImage from "../../../core/components/AvatarImage";
+import ThemedSVG from "../../../core/components/ThemedSVG";
+import { Player } from "../../../core/definitions";
+import { GenderLabels } from "../../../core/utils/static";
+import globalStyles from "../../../core/utils/styles";
+
+import { frameHeight } from "../PlayerBattlePanel/styles";
+
+// @ts-ignore
+import CallAlly from "../../../../../assets/icons/CallAlly.svg";
+import {battleTheme} from "../../../core/utils/themes";
+
+interface AvatarProps {
+  player?: Player;
+  isAlly?: boolean;
+}
+
+const avatarSize = frameHeight * 0.36;
+
+/**
+ * Avatar documentation
+ */
+const Fighter: React.FunctionComponent<AvatarProps> = ({ player, isAlly }) => {
+  return (
+    <View style={styles.container}>
+      {isAlly && !player && (
+        <Fragment>
+          <TouchableOpacity>
+            <ThemedSVG
+              SVGImage={CallAlly}
+              height={avatarSize}
+              width={avatarSize}
+							theme={battleTheme}
+            />
+          </TouchableOpacity>
+          <Text style={styles.infoText}>Help!</Text>
+        </Fragment>
+      )}
+      {player && (
+        <Fragment>
+          <AvatarImage
+            width={avatarSize}
+            height={avatarSize}
+            index={player!.memberInfo.avatar}
+            theme={player!.memberInfo.theme}
+          />
+          <View style={styles.infoContainer}>
+            <Text style={styles.infoText}>
+              lvl {player!.level} - {GenderLabels[player!.inGameGender]}
+            </Text>
+          </View>
+        </Fragment>
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {},
+  infoContainer: {
+    marginTop: 2,
+  },
+  infoText: {
+    ...globalStyles.text,
+  },
+});
+
+export default Fighter;
