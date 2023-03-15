a7e556c96d0dc1bd7de8549f2cc7f1e2d83c4e09
Dahan Schuster <dan.plschuster@gmail.com>
2023-03-15 02:01:45 -0300

    feat: Hides overflow content inside the Battle panel

--- src/modules/battle/containers/PlayerBattlePanel/styles.ts
+++ src/modules/battle/containers/PlayerBattlePanel/styles.ts
@@ -4,25 +4,29 @@ import { vw } from "../../../core/utils/styles";
 export const frameWidth = vw(90);
 export const frameHeight = frameWidth * 0.63;
 
+export const frameContentWidth = frameWidth * 0.96;
+export const frameContentHeight = frameHeight * 0.6;
+
 export const iconsSize = frameHeight * 0.36;
 
 const styles = StyleSheet.create({
-	container: {
-		position: "relative",
-		width: frameWidth, 
-		height: frameHeight,
-		justifyContent: 'center',
-		alignItems: 'center',
-	},
-	content: {
-		width: frameWidth * 0.96, 
-		height: frameHeight * 0.6,
-		paddingHorizontal: 8,
-		paddingVertical: 8,
-		flexDirection: 'row',
-		justifyContent: 'space-evenly',
-		alignItems: 'center',
-	}
+  container: {
+    position: "relative",
+    width: frameWidth,
+    height: frameHeight,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  content: {
+    width: frameContentWidth,
+    height: frameContentHeight,
+    paddingHorizontal: 8,
+    paddingVertical: 8,
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    overflow: "hidden",
+  },
 });
 
 export default styles;
