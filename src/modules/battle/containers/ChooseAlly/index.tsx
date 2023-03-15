9cacdf3b1a385ed94671318a83435b1da69c7f27
Dahan Schuster <dan.plschuster@gmail.com>
2023-03-15 02:23:19 -0300

    feat: Automatic selection of the snapped ally

--- src/modules/battle/containers/ChooseAlly/index.tsx
+++ src/modules/battle/containers/ChooseAlly/index.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useCallback, useState } from "react";
+import React, { Fragment, useCallback, useMemo, useState } from "react";
 import { Text, TouchableOpacity } from "react-native";
 import ThemedSVG from "../../../core/components/ThemedSVG";
 
@@ -6,37 +6,66 @@ import ThemedSVG from "../../../core/components/ThemedSVG";
 import CallAlly from "../../../../../assets/icons/CallAlly.svg";
 import globalStyles from "../../../core/utils/styles";
 import { battleTheme } from "../../../core/utils/themes";
-import {
-  iconsSize,
-} from "../PlayerBattlePanel/styles";
+import { iconsSize } from "../PlayerBattlePanel/styles";
 import { interpolate } from "react-native-reanimated";
 import Carousel from "react-native-reanimated-carousel";
 import AvatarImage from "../../../core/components/AvatarImage";
 import useGame from "../../../core/hooks/useGame";
 import { useBattle } from "../../contexts/BattleContext";
-import {Player} from "../../../core/definitions";
+import { Player } from "../../../core/definitions";
+import useInterval from "../../../core/hooks/useInterval";
 
 interface ChooseAllyProps {
   // TODO: Component props
 }
 
+// default time to wait to go back to idle state after stop
+// scrolling the carousel
+const BACK_TO_IDLE_TIMEOUT = 2000;
+
 /**
  * ChooseAlly documentation
  */
 const ChooseAlly: React.FunctionComponent<ChooseAllyProps> = () => {
-  const [isChoosing, setIsChoosing] = useState<boolean>(false);
-
-  const RIGHT_OFFSET = -24;
-
   const { playerList } = useGame();
   const { mainPlayer, setAllyPlayer } = useBattle();
 
-	const handleSelectAlly = useCallback((ally: Player) => {
-		setAllyPlayer(ally);
-		setIsChoosing(false);
-	}, [])
+  // list of available allies, doesn't include the current main player of the battle
+  const availableAllies = useMemo(
+    () => playerList.filter((p) => p.id !== mainPlayer?.id),
+    [playerList, mainPlayer]
+  );
 
+  // item that has been snapped in the carousel, for automatic selection
+  // after leaving the carousel idling
+  const [snappedIndex, setSnappedIndex] = useState<number>(0);
 
+  const [isChoosing, setIsChoosing] = useState<boolean>(false);
+  const [backToIdleTimeout, setBackToIdleTimeout] = useState<number | null>(
+    null
+  );
+
+  /**
+   * Set up a react interval that changes the battle state
+   * back to idle and removes itself (setting the timout to null)
+   *
+   * To activate the interval, set a timeout in ms
+   */
+  useInterval(
+    useCallback(() => {
+			handleSelectAlly(availableAllies[snappedIndex]!)	
+      setIsChoosing(false);
+      setBackToIdleTimeout(null);
+    }, [snappedIndex]),
+    backToIdleTimeout
+  );
+
+  const handleSelectAlly = useCallback((ally: Player) => {
+    setAllyPlayer(ally);
+    setIsChoosing(false);
+  }, []);
+
+  const RIGHT_OFFSET = -24;
   const animationStyle = useCallback(
     (value: number) => {
       "worklet";
@@ -82,13 +111,16 @@ const ChooseAlly: React.FunctionComponent<ChooseAllyProps> = () => {
             justifyContent: "center",
             width: iconsSize,
             height: iconsSize * 1.4,
-						borderRadius: 24,
+            borderRadius: 24,
           }}
           width={iconsSize}
           pagingEnabled={false}
           height={iconsSize}
           customAnimation={animationStyle}
-          data={playerList.filter((p) => p.id !== mainPlayer?.id)}
+          data={availableAllies}
+          onProgressChange={() => setBackToIdleTimeout(BACK_TO_IDLE_TIMEOUT)}
+          onScrollBegin={() => setBackToIdleTimeout(null)}
+          onSnapToItem={(index) => setSnappedIndex(index)}
           renderItem={({ item, index }) => (
             <TouchableOpacity onPress={() => handleSelectAlly(item)}>
               <AvatarImage
