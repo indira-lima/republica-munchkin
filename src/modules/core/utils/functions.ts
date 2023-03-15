e179f4854b71df2b65097b364ce061ede2edd2bf
Dahan Schuster <dan.plschuster@gmail.com>
2023-03-15 03:02:14 -0300

    feat: Darken function for darkening a hex color

--- src/modules/core/utils/functions.ts
+++ src/modules/core/utils/functions.ts
@@ -1,20 +1,15 @@
-import { NavigationProp } from '@react-navigation/native'
-
 /**
  * Ordena um array de objetos usando a chave especificada
  */
 // @ts-expect-error TS(2551): Property 'sortBy' does not exist on type 'any[]'. ... Remove this comment to see the full error message
 if (!Array.prototype.sortBy) {
-  Object.defineProperty(Array.prototype, 'sortBy', {
-  // @ts-expect-error TS(7006): Parameter 'key' implicitly has an 'any' type.
-  	value: function(key, order = "asc") {
-  		return [].concat(this).sort(
-  // @ts-expect-error TS(2345): Argument of type '(a: never, b: never) => boolean'... Remove this comment to see the full error message
-  			order === "desc" ?
-  			(a, b) => a[key] < b[key] :
-  			(a, b) => a[key] > b[key]
-  		)
-  	}
+  Object.defineProperty(Array.prototype, "sortBy", {
+    value: function (key: string, order = "asc") {
+      return [].concat(this).sort(
+        // @ts-expect-error TS(2345): Argument of type '(a: never, b: never) => boolean'... Remove this comment to see the full error message
+        order === "desc" ? (a, b) => a[key] < b[key] : (a, b) => a[key] > b[key]
+      );
+    },
   });
 }
 
@@ -23,60 +18,31 @@ if (!Array.prototype.sortBy) {
  */
 // @ts-expect-error TS(2339): Property 'last' does not exist on type 'any[]'.
 if (!Array.prototype.last) {
-  Object.defineProperty(Array.prototype, 'last', {
-    value: function() {
-        return this[this.length - 1];
-    }
+  Object.defineProperty(Array.prototype, "last", {
+    value: function () {
+      return this[this.length - 1];
+    },
   });
 }
 
-/**
- * 
- * @param { NavigationProp } navigation 
- */
-// @ts-expect-error TS(7006): Parameter 'navigation' implicitly has an 'any' typ... Remove this comment to see the full error message
-export function resetNavigationToMenu(navigation) {
-	if (!navigation.reset)
-		throw new Error("Objeto navigation inválido")
+function darkenColor(color: string, amount: number): string {
+  // Convert hex color to rgb
+  let r = parseInt(color.substring(1, 2), 16);
+  let g = parseInt(color.substring(3, 2), 16);
+  let b = parseInt(color.substring(5, 2), 16);
 
-	navigation.reset({
-		index: 0,
-		routes: [{ name: 'Menu' }],
-	})
+  // Calculate darker rgb values
+  r = Math.round(r * (1 - amount / 100));
+  g = Math.round(g * (1 - amount / 100));
+  b = Math.round(b * (1 - amount / 100));
+
+  // Convert darker rgb values to hex
+  color = "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+
+  return color;
 }
 
-/**
- * Verifica se a ação a ser despachada pelo navigation
- * é um reset para o Menu
- * 
- * Esta ação é despacahada ao clicar no botão Home da
- * tab bar, que chama a função resetNavigationToMenu
- * 
- * @param {object} action objeto Action a ser despachado
- * @returns 
- */
-// @ts-expect-error TS(7006): Parameter 'action' implicitly has an 'any' type.
-export function isResetingToMenu(action) {
-	return action?.type === "RESET" &&
-		   action?.payload?.routes[0]?.name === "Menu"
-}
-
-/**
- * Substitui vírgulas na string `numero` para pontos e a
- * retorna convertida para number
- * 
- * @param {string} numero um número escrito com vírgula
- * @returns o número decimal convertido
- */
-// @ts-expect-error TS(7006): Parameter 'numero' implicitly has an 'any' type.
-export function numeroVirgulaParaPonto(numero) {
-	return +String(numero).replace(/,/, '.');
-}
-
-/**
- * Import all resources from a require.context
- */
-// @ts-expect-error TS(7006): Parameter 'r' implicitly has an 'any' type.
-export function importAll(r) {
-	r.keys().forEach(r)
+function componentToHex(c: number): string {
+  let hex = c.toString(16);
+  return hex.length == 1 ? "0" + hex : hex;
 }
