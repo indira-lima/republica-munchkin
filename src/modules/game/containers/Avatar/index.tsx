cc0a207fac6e73e504ca9ab802bdde4679b90bbe
Dahan Schuster <dan.plschuster@gmail.com>
2023-03-22 14:56:10 -0300

    refact: Uses DoubleTapButton at PlayerAvatar component

--- src/modules/game/containers/Avatar/index.tsx
+++ src/modules/game/containers/Avatar/index.tsx
@@ -1,30 +1,21 @@
-import { useCallback, useState } from "react";
-import { StyleSheet, TouchableWithoutFeedback, View } from "react-native";
+import { useCallback } from "react";
+import { StyleSheet, View } from "react-native";
 import { useNavigation } from "@react-navigation/native";
-import Animated, {
-  FadeIn,
-  FadeOut,
-  FlipInEasyY,
-  FlipOutEasyY,
-} from "react-native-reanimated";
 
 import { Player } from "../../../core/definitions";
 import globalStyles, { circulo } from "../../../core/utils/styles";
 
 // @ts-ignore
 import BattleReady from "../../../../../assets/icons/BattleReady.svg";
-import useInterval from "../../../core/hooks/useInterval";
 import AvatarImage from "../../../core/components/AvatarImage";
 import ThemedSVG from "../../../core/components/ThemedSVG";
+import DoubleTapButton from "../../../core/containers/DoubleTapButton";
 
 interface PlayerAvatarProps {
   player: Player;
   enableEdit?: boolean;
   onChange?: (newAvatarIndex: number) => void;
 }
-
-// default time to wait to go back to idle state after the first tap
-const BACK_TO_IDLE_TIMEOUT = 2000;
 
 /**
  * Component used to show the player's avatar image
@@ -36,72 +27,36 @@ const PlayerAvatar: React.FunctionComponent<PlayerAvatarProps> = ({
 }) => {
   const navigation = useNavigation();
 
-  // battle state: is the player ready for some action??
-  const [battleState, setBattleState] = useState<"idle" | "ready">("idle");
-  const [backToIdleTimeout, setBackToIdleTimeout] = useState<number | null>(
-    null
-  );
-
   /**
-   * Set up a react interval that changes the battle state
-   * back to idle and removes itself (setting the timout to null)
-   *
-   * To activate the interval, set a timeout in ms
+   * Redirects to Battle screen
+   * Called after the second tap at the DoubleTapButton
    */
-  useInterval(() => {
-    setBattleState("idle");
-    setBackToIdleTimeout(null);
-  }, backToIdleTimeout);
-
-  /**
-   * Cycle the battle state trough idle and ready,
-   * redirecting to Battle screen when ready
-   */
-  const handleBattleStateChange = useCallback(() => {
-    if (battleState === "idle") {
-      setBattleState("ready");
-      setBackToIdleTimeout(BACK_TO_IDLE_TIMEOUT); // 2s timeout
-
-      return;
-    }
+  const onConfirmBattle = useCallback(() => {
     // @ts-ignore
     navigation.navigate("Battle", { player });
-    setBattleState("idle");
-  }, [battleState, player]);
+  }, [navigation, player]);
 
   return (
     <View style={styles.container}>
-      {/* If edition is enabled, renders two buttons on the side to change the avatar */}
-      <TouchableWithoutFeedback onPress={handleBattleStateChange}>
-        <View>
-          {/* Idle means the the user hasn't tapped the avatat */}
-          {battleState === "idle" && (
-            <Animated.View entering={FlipInEasyY} exiting={FlipOutEasyY}>
-              <AvatarImage
-                index={player.memberInfo.avatar}
-                width={avatarSize}
-                height={avatarSize}
-                theme={player.memberInfo.theme}
-              />
-            </Animated.View>
-          )}
-          {/* If the user does tap the avatar, the battle icon is shown, ready for battle */}
-          {battleState === "ready" && (
-            <Animated.View
-              entering={FadeIn}
-              exiting={FadeOut}
-              style={styles.battleImageWrapper}
-            >
-              <ThemedSVG
-                theme={player.memberInfo.theme}
-								SVGImage={BattleReady}
-                width={avatarSize}
-                height={avatarSize}
-              />
-            </Animated.View>
-          )}
-        </View>
-      </TouchableWithoutFeedback>
+      <DoubleTapButton
+        idleChildren={
+          <AvatarImage
+            index={player.memberInfo.avatar}
+            width={avatarSize}
+            height={avatarSize}
+            theme={player.memberInfo.theme}
+          />
+        }
+        confirmChildren={
+          <ThemedSVG
+            theme={player.memberInfo.theme}
+            SVGImage={BattleReady}
+            width={avatarSize}
+            height={avatarSize}
+          />
+        }
+        onConfirm={onConfirmBattle}
+      />
     </View>
   );
 };
